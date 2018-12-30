8cc70e657c11661af13123cd738d91072d427e4a
DanielChen1234 <dchen8492@yahoo.com>
2018-12-30 16:31:44 -0500

    Updates

---
+++ precache-manifest.afdd424d540c6f4da2a49a1c44105e2a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "60ebdd6b1870a2709c31",
+    "url": "/personal-site/static/js/runtime~main.60ebdd6b.js"
+  },
+  {
+    "revision": "4bd28e2fa1f290db4d9a",
+    "url": "/personal-site/static/js/main.4bd28e2f.chunk.js"
+  },
+  {
+    "revision": "67aeea0ef3cf26ecd532",
+    "url": "/personal-site/static/js/1.67aeea0e.chunk.js"
+  },
+  {
+    "revision": "4bd28e2fa1f290db4d9a",
+    "url": "/personal-site/static/css/main.71fe3c2a.chunk.css"
+  },
+  {
+    "revision": "67aeea0ef3cf26ecd532",
+    "url": "/personal-site/static/css/1.4d160ddc.chunk.css"
+  },
+  {
+    "revision": "2fc55460bae27fb60e1c0a788c0426db",
+    "url": "/personal-site/index.html"
+  }
+];
